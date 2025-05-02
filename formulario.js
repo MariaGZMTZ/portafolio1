9aadf75411dbf0965b16b4f0c75118249472666f
MariaGZMTZ <zmariaguadalupe63@gmail.com>
2025-05-01 23:15:47 -0600

    Delete formulario.js

--- formulario.js
+++
@@ -1,9 +0,0 @@
-function leer(){
-	//referencia por pseudoclase
-	var nombre=document.forms["formulario"].elements[0].value;
-	//referencia por Id
-	var clave=document.getElementById("pass").value;
-	//referencia por TagName
-	var car=document.getElementByTagName("select")[0].value;
-	document.getElementById("datos").innerHTML="Nombre: "+nombre+ "\<br>Password: "+clave+ "\<br>Tu carrera es: "+car;
-}
